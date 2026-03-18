489788e36a24db9476017dc0df7bce8abca0bc55
Harshit Mani Tripathi <harshit@hudater.dev>
2026-03-18 12:43:09 +0530

    finally fixed the last grid logic

---
+++ script.js
@@ -0,0 +1,13 @@
+const ol = document.querySelector('.blogs-ol');
+const items = [...ol.querySelectorAll('.blog-li')];
+
+if (items.length % 4 === 3) {
+    const last3 = items.slice(-3);
+    const wrapper = document.createElement('div');
+    wrapper.className = 'three-orphans-row';
+    ol.appendChild(wrapper);
+    last3.forEach(li => {
+        li.classList.add('orphan-item');  // ye add kiya
+        wrapper.appendChild(li);
+    });
+}
